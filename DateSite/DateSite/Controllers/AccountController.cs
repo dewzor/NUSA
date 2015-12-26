d6ba7ccd17d39408fd32481f2ba44471b1a1171f
dewzor <bergla84@hotmail.com>
2015-12-26 21:35:14 +0100

    hej

---
+++ DateSite/DateSite/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DateSite.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public UsersRepository _usersRepository = new UsersRepository();
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string username, string password, string firstname, string lastname, string email, string gender, string age, string location, string about)
+        {
+
+            Profiles profile = new Profiles();
+            profile.Username = username;
+            profile.Pass = password;
+            profile.Firstname = firstname;
+            profile.Lastname = lastname;
+            profile.Gender = gender;
+            profile.Age = Int32.Parse(age);
+            profile.Location = location;
+            profile.About = about;
+            profile.Email = email;
+    
+
+            _usersRepository.insertUser(profile);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ DateSite/Repositories/UsersRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class UsersRepository
+    {
+
+
+        /// <summary>
+        /// Hämtar alla användare
+        /// </summary>
+        public List<Profiles> fetchProfiles()
+        {
+            using (var context = new UserDBEntities())
+            {
+                context.Database.Connection.Open();
+                return context.Profiles.ToList();
+            }
+        }
+
+
+        /// <summary>
+        /// Lägger till en användare i databasen
+        /// </summary>
+        public void insertUser(Profiles profile)
+        {
+            try
+            {
+                using (var context = new UserDBEntities())
+                {
+                    context.Database.Connection.Open();
+                    context.Profiles.Add(profile);
+                    context.SaveChanges();
+                }
+            }
+            catch(Exception e)
+            {
+                
+            }
+
+        }
+    }
+}
